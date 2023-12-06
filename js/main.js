4eb35476186aa6cf15525843ae1fe5de90993d63
Constantin <xlikeLK@gmail.com>
2023-12-06 05:58:03 +0200

    adaptiv 768 and 3200 , for  2 first section

--- js/main.js
+++ js/main.js
@@ -13,9 +13,38 @@ $(function () {
   $("#birthday").on("input", function () {
     changePlaceholderColor(this);
   });
-
+  // change place holder inputcolor
   function changePlaceholderColor(input) {
     input.setAttribute("placeholder", "dd/mm/yy");
     input.style.color = "#000";
   }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Получаем кнопку "Continue" на первом шаге
+    var continueButton = document.querySelector(
+      ".form__step:first .form-btn button"
+    );
+
+    // Добавляем обработчик события клика на кнопку "Continue"
+    continueButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      console.log("continueButton");
+
+      // Предотвращаем действие по умолчанию (например, отправку формы)
+
+      // Убираем класс активного шага с первого и добавляем к следующему
+      document
+        .querySelector(".form__step:first")
+        .classList.remove("form__step--active");
+      document
+        .querySelector(".form__step:nth-child(2)")
+        .classList.add("form__step--active");
+
+      // Изменяем стиль для шкалы прогресса
+      var progressBar = document.querySelector(
+        ".form__bars-line--first::before"
+      );
+      progressBar.style.width = "100%";
+    });
+  });
 });
